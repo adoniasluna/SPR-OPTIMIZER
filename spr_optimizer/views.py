f132215248808ea647af7b48032234ec5371696f
adoniasluna <alps@ecomp.poli.br>
2019-02-10 00:00:24 -0300

    formating/cleaning code

--- spr_optimizer/views.py
+++ spr_optimizer/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse
 from facade import facade
 
 
-
 def index(request):
     template = loader.get_template('spr_optimizer/index.html')
     context = {}
@@ -15,16 +14,17 @@ def index(request):
 
 
 def upload(request):
-    template = loader.get_template('spr_optimizer/upload.html')
     file_list = []
     date = datetime.now()
 
     for count, x in enumerate(request.FILES.getlist("file_opt")):
         file_name = hashlib.sha224(str(date).encode('utf-8')).hexdigest() + str(count)
         facade.upload_files("media/" + str(file_name) + ".txt", x)
-        file_list.append(file_name +'-'+  str(x))
+        file_list.append(file_name + '-' + str(x))
 
-    #the context has all the files names so it could be show at the index page
+    template = loader.get_template('spr_optimizer/upload.html')
+
+    # the context has all the files names so it could be show at the index page
     context = {"files": file_list}
 
     return HttpResponse(template.render(context, request))
@@ -37,16 +37,9 @@ def optimize(request):
     files_system_name = [elem.split("-")[0] + '.txt' for elem in files]
 
     best_position = facade.optimize(files_system_name)
+    context = {"results": best_position}
 
     template = loader.get_template('spr_optimizer/result.html')
-    files = []
-    for key, value in request.POST.items():
-        if key == 'file_uploads':
-            files.append(value.split("-")[0]+'.txt')
-    best_position = facade.optimize(files)
-
-    context = {"results": best_position}
-    print(best_position)
     return HttpResponse(template.render(context, request))
 
 
