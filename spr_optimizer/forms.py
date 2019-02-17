c817c793a217f456bf8556c9d436464a2e5badaa
adoniasluna <alps@ecomp.poli.br>
2019-02-17 17:59:16 -0300

    updating forms fields

--- spr_optimizer/forms.py
+++ spr_optimizer/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
 
 class PSOSettingForm(forms.Form):
     particle_number = forms.IntegerField(initial=500, min_value=20, max_value=500)
     iteration_number = forms.IntegerField(initial=10, max_value=100)
-    inertia_weight = forms.FloatField(initial=0.5, min_value=0)
+    inertia_weight = forms.FloatField(initial=0.5, min_value=0, max_value=1.4)
     cognitive_constant = forms.FloatField(initial=2.04, min_value=0)
     social_constant = forms.FloatField(initial=2.04, min_value=0)
-    initial_position = forms.CharField(initial="random")
+
+    positions_choices = [("RANDOM", "Random"), ("APPROXIMATED", "Approximated")]
+    initial_position = forms.ChoiceField(choices=positions_choices, initial="RANDOM")
